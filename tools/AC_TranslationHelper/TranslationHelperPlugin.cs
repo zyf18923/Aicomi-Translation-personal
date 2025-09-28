9ae05c94447ab601ddaeb0852517db034c899ff0
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-09-28 20:36:46 +0200

    hscene and freeh translation improvements

--- tools/AC_TranslationHelper/TranslationHelperPlugin.cs
+++ tools/AC_TranslationHelper/TranslationHelperPlugin.cs
@@ -1,11 +1,14 @@
-﻿using System.Reflection;
-using AC_TranslationHelper;
+﻿using AC.Scene.FreeH.CharaStateSelect;
+using AC.Scene.Home.UI;
 using AC.UI;
+using AC_TranslationHelper;
 using BepInEx;
 using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
 using Character;
 using HarmonyLib;
+using System.Reflection;
+using TMPro;
 using XUnity.AutoTranslator.Plugin.Core;
 
 [assembly: AssemblyTitle(TranslationHelperPlugin.DisplayName)]
@@ -22,12 +25,51 @@ namespace AC_TranslationHelper
         public const string Version = "0.1";
         public const string GUID = "TranslationHelper";
         internal const string DisplayName = "Translation Helper";
-        internal static ManualLogSource Logger;
+        internal static ManualLogSource Logger = null!;
 
         public override void Load()
         {
             Logger = Log;
             Harmony.CreateAndPatchAll(typeof(Hooks));
+        }
+
+        private static string TryTranslateName(string firstname, string lastname, char separator)
+        {
+            var name = new Il2CppSystem.ValueTuple<string, string>(firstname, lastname);
+            TryTranslateName(ref name);
+            return name.Item1 + (string.IsNullOrWhiteSpace(name.Item2) ? "" : separator + name.Item2);
+        }
+
+        private static bool TryTranslateName(ref Il2CppSystem.ValueTuple<string, string> __result)
+        {
+            var result = false;
+
+            if (!string.IsNullOrWhiteSpace(__result.Item1) && AutoTranslator.Default.TryTranslate(__result.Item1, out var tl))
+            {
+                __result.Item1 = tl;
+                result = true;
+            }
+
+            if (!string.IsNullOrWhiteSpace(__result.Item2) && AutoTranslator.Default.TryTranslate(__result.Item2, out var tl2))
+            {
+                __result.Item2 = tl2;
+                result = true;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Workaround for names in roster screen and some other things not getting translated until a manual translation reload.
+        /// Sometime AT misses the text update if it happens purely on the il2cpp side, this makes AT re-check the text
+        /// </summary>
+        private static void Touch(params TMP_Text?[] texts)
+        {
+            foreach (var text in texts)
+            {
+                if (text != null)
+                    text.text = text.text;
+            }
         }
 
         private static class Hooks
@@ -39,33 +81,49 @@ namespace AC_TranslationHelper
             [HarmonyPatch(typeof(HumanDataParameter), nameof(HumanDataParameter.fullname), MethodType.Getter)]
             public static void Postfix_fullname_get(HumanDataParameter __instance, ref string __result)
             {
-                string? first = null;
-                string? last = null;
-                if ((string.IsNullOrWhiteSpace(__instance.firstname) || AutoTranslator.Default.TryTranslate(__instance.firstname, out first)) &&
-                    (string.IsNullOrWhiteSpace(__instance.lastname) || AutoTranslator.Default.TryTranslate(__instance.lastname, out last)))
-                {
-                    var original = __result;
+                var newName = TryTranslateName(__instance.firstname, __instance.lastname, ' ');
 
-                    __result = first ?? "";
-                    if (!string.IsNullOrWhiteSpace(last))
-                    {
-                        if (!string.IsNullOrWhiteSpace(__result))
-                            __result += " ";
-                        __result += last;
-                    }
+                Logger.LogDebug($"Fullname translated: {__result} -> {newName}");
 
-                    Logger.LogDebug($"Fullname translated: {original} -> {__result}");
-                }
+                __result = newName;
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(BaseStateSelect), nameof(BaseStateSelect.GetName))]
+            public static void Postfix_BaseStateSelect_GetName(HumanData data, ref Il2CppSystem.ValueTuple<string, string> __result)
+            {
+                System.Diagnostics.Debug.Write("Postfix_BaseStateSelect_GetName");
+                TryTranslateName(ref __result);
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(BaseStateSelect), nameof(BaseStateSelect.Set))]
+            public static void Postfix_BaseStateSelect_Set(BaseStateSelect __instance, HumanData? data)
+            {
+                System.Diagnostics.Debug.Write("Postfix_BaseStateSelect_Set");
+
+                if (data == null) return;
+
+                var tmp = __instance._txtName.GetTmpText();
+                if (tmp == null) return;
+
+                tmp.text = TryTranslateName(data.Parameter.firstname, data.Parameter.lastname, '\n');
             }
 
             [HarmonyPostfix]
             [HarmonyPatch(typeof(Passport), nameof(Passport.SetParameter))]
-            public static void Postfix_fullname_get(Passport __instance, AC.User.ActorData data)
+            public static void Postfix_Passport_SetParameter(Passport __instance)
             {
-                // Workaround for names in roster screen not getting translated.
-                // Seems like AT misses the text update, this makes AT re-check the text
-                if (__instance._txtName != null)
-                    __instance._txtName.text = __instance._txtName.text;
+                System.Diagnostics.Debug.Write("Postfix_Passport_SetParameter");
+                Touch(__instance._txtName);
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(HumanParameterUI), nameof(HumanParameterUI.Refresh))]
+            public static void Postfix_HumanParameterUI_Refresh(HumanParameterUI __instance)
+            {
+                System.Diagnostics.Debug.Write("Postfix_HumanParameterUI_Refresh");
+                Touch(__instance._txtActivity, __instance._txtBirthday, __instance._txtBloodType, __instance._txtCallsign, __instance._txtErogenousZone, __instance._txtPersonality);
             }
         }
     }
